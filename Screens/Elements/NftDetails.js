804974409abfd5a826ed2ac62d790e491713bda7
Dzhambulat Khasaev <dzkhasayev@gmail.com>
2022-12-20 22:42:11 +0300

    fix details design

--- Screens/Elements/NftDetails.js
+++ Screens/Elements/NftDetails.js
@@ -4,29 +4,39 @@ function NftDetails({ navigation, route }) {
     const nftInfo = route.params.details;
     return (
         <SafeAreaView style={{ flex: 1, flexDirection: "column", alignItems: "center", justifyContent: "start" }}>
-            <View style={{ marginTop: "20px", width: "100%", height: "40px", flexDirection: "row", alignItems: "flex-start", justifyContent: "flex-start" }}>
-
-                <View styles={{ width: "40px" }}>
+            <View style={{ zIndex: 5, position: "absolute", left: "20px", top: "20px" }}>
+                <View>
                     <TouchableOpacity onPress={navigation.goBack}>
-                        <Ionicons styles={{ marginLeft: "20px", height: "40px", width: "40px" }} name="arrow-back-outline" color="#0a8258" />
+                        <Ionicons style={{ fontSize: "40px", opacity: "0.7" }} name="arrow-back-circle-outline" color="white" />
                     </TouchableOpacity>
                 </View>
-
             </View>
-            <View style={{ flex: 1 }}>
+            <View style={{ flex: 1, width: "100%" }}>
                 <Image
                     source={nftInfo.tokenUri}
                     resizeMode="cover"
                     style={{
-                        height: "100px",
-                        width: "100px",
-                        borderRadius: "50%",
-                        marginTop: "15px"
+                        height: "300px",
+                        width: "100%"
                     }}
                 />
-                <Text>
-                    {nftInfo.nftName}
-                </Text>
+                <View style={{ padding: "5px", marginTop: "10px", marginLeft: "5px", marginRight: "5px", backgroundColor: "white", borderRadius: "5px" }}>
+
+                    <View style={{ flexDirection: "row", justifyContent: "space-between", borderBottomColor: "#0a8258", borderBottomWidth: "1px" }}>
+                        <Text style={{ color: "#0a8258" }}>NFT label</Text>
+                        <Text style={{ color: "black" }}>
+                            {nftInfo.nftName}
+                        </Text>
+                    </View>
+                    <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+                        <Text style={{ color: "#0a8258" }}>NFT URI</Text>
+                        <Text style={{ color: "black" }}>
+                            {nftInfo.tokenUri}
+                        </Text>
+                    </View>
+
+                </View>
+
             </View>
 
         </SafeAreaView>
